01d1f3f0131b218981ef17773120893040247e9f
jovanes-react <jovanes.react.js@gmail.com.com>
2024-08-23 21:07:33 -0300

    terminamos con useEffect

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,21 +1,84 @@
+import { useEffect, useState } from "react";
 import { products } from "../../constants/products";
 import Product from "./Product/Product";
+// import bgStyleTypes from "../../config/bgStyleTypes.json"
 
 import "./styles.css";
 
 const Products = () => {
+
+  //bgStyleTypes.neutro => bg-neutro
+  //bgStyleTypes["neutro"] => bg-neutro
+  // const {neutro} = bgStyleTypes
+
+  // 1) player.name 
+  // 2) player["name"] => Messi 
+  // 3) const {name} = player
+
+
+
+
+  // stado => loading , animales 
+  // loading true 
+  // animales []
+  // peticion 
+  // loading false 
+  // animales ["leon","bufalo"]
+  // boton refresh 
+  // animales []
+  // loading true 
+  // lanzo la peticion 
+  // animales ["leon","gato","caballo"]
+  // loading false 
+
+
+
+
+  // cargo => useEffect => change(bg-neutro => bg-light) => se muere (hay que mostrar con fondo blanco) => vuelve a cargar pero ya con fondo blanco (bg-light)
+
+
+
+// appMode ? bg-neutro => 3s despues bg-light 
+// appMode ? bg-light => 3s despues bg-light ? 
+
+const [appMode,setAppMode] = useState("bg-neutro")
+const [darkMode,setDarkMode] = useState(false)
+
+
+useEffect(() => {
+  // el componente se renderizo correctamente ⬇️
+  // mount 
+  if(darkMode){
+    setAppMode('bg-dark')
+  }else {
+      setAppMode('bg-light')
+    // setTimeOut = metodo de javascript que hace esperar  x cantidad de tiempo antes de ser ejecutado
+  }
+}, [ darkMode ]) // watch , changes, => estar atento a estos valores 
+
+// appMode !== bg-light ? "sol" : "luna" 
+
+//version antigua 
+// const handleSetDarModeTrue = () => setDarkMode(true)
+// const handleSetDarkModeFalse = () => setDarkMode(false)
+
+// = asignacion (valor cualquiera )
+// == ? comparacion => booleano
+// === ? comparacion => booleano
+// !== ? comparacion => booleano 
+
+// version nueva 
+const handleToggleDarkMode = () => setDarkMode(!darkMode)
+
   return (
-    <div className="container">
+    <div className={`container ${appMode}`}>
+      <div>
+        <button style={{fontSize:"40px",padding:"10px 30px"}} onClick={handleToggleDarkMode}>{ darkMode ? "🌞" : "🌙" }</button>
+      </div>
       {products.map((producto) => (
         <Product 
         key={producto.id}
         {...producto}
-        // name={producto.name}
-        // marca={producto.marca}
-        // img={producto.img}
-        // precio={producto.precio}
-        // disponible={producto.disponible}
-        // descuento={producto.descuento}
         />
       ))}
     </div>
